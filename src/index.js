4d884256464eb76972ffb1132a493401602b71c0
patil-pratik-87 <pratikvpatil0@gmail.com>
2021-10-02 18:39:08 +0530

    Virtual DOM Clock vs Vanilla JS Clock Comparison

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './style.css';
+// inspect the element in dev tools for change. You will notice only the required tag changing rather than the entire DOM.
+// Enable Rendering >> Paint Flashing to see DOM updates on  screen
 
 function tick() {
+  // Vaniall Js Clock
+  document.getElementById('js-element').innerHTML = `
+    <div class="container">
+      <h1> Javascript Clock </h1>
+      <div class="clock">
+        <h2>${new Date().toLocaleTimeString()}</h2>
+      </div>
+    </div>`;
+
+  // React Clock
   const element = (
-    <div>
-      <h2>The time is {new Date().toLocaleTimeString()}.</h2>
+    <div className="container">
+      <h1> React Clock </h1>
+      <div className="clock">
+        <h2>{new Date().toLocaleTimeString()}</h2>
+      </div>
     </div>
   );
-  // highlight-next-line
-  ReactDOM.render(element, document.getElementById('root'));
+  ReactDOM.render(element, document.getElementById('react-element'));
 }
 
 setInterval(tick, 1000);
-
-// vs
